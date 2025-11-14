a1b59fa1fe8bcf105b30427f8d829fef542e0bb3
Mvp-ap <apostolosalexandris39@gmail.com>
2025-11-14 09:40:27 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import BookingApp from "./BookingApp";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 text-gray-900 p-4">
+      <BookingApp />
+    </div>
+  );
+}
